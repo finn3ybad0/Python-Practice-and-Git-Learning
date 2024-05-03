0209961b1df1f9888b47ef183cbc6a02797fa73b
Finney Bado <finney.bado@icloud.com>
2024-05-03 15:08:31 -0400

    exercice2

---
+++ calculatorV1.py
@@ -0,0 +1,17 @@
+# Name : Finney Bado
+# Date : 03/05/2024
+# Description : This is a simple calculator that adds 2 numbers given as input by the user and displays it
+
+
+
+def calculatorVI():
+
+    print("------ CALCULATOR V-I -----")
+
+    number1 = int(input("enter the first number: "))
+    number2 = int(input("enter the second number: "))
+
+    print(f"result : {number1+number2}")
+
+
+calculatorVI()
